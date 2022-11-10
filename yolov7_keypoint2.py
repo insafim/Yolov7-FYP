3222f5ef6db5c12108bd35eb51797bc43070f258
insafim <50768908+insafim@users.noreply.github.com>
2022-11-10 12:08:14 +0530

    Insaf 10.11.2022

---
+++ yolov7_keypoint2.py
@@ -0,0 +1,57 @@
+import torch
+from torchvision import transforms
+
+from utils.datasets import letterbox
+from utils.general import non_max_suppression_kpt
+from utils.plots import output_to_keypoint, plot_skeleton_kpts
+
+import matplotlib.pyplot as plt
+import cv2
+import numpy as np
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+def load_model():
+    model = torch.load('yolov7-w6-pose.pt', map_location=device)['model']
+    # Put in inference mode
+    model.float().eval()
+
+    if torch.cuda.is_available():
+        # half() turns predictions into float16 tensors
+        # which significantly lowers inference time
+        model.half().to(device)
+    return model
+
+model = load_model()
+
+def run_inference(image):
+    # Resize and pad image
+    image = letterbox(image, 960, stride=64, auto=True)[0] # shape: (567, 960, 3)
+    # Apply transforms
+    image = transforms.ToTensor()(image) # torch.Size([3, 567, 960])
+    if torch.cuda.is_available():
+      image = image.half().to(device)
+    # Turn image into batch
+    image = image.unsqueeze(0) # torch.Size([1, 3, 567, 960])
+    with torch.no_grad():
+      output, _ = model(image)
+    return output, image
+
+def draw_keypoints(output, image):
+  output = non_max_suppression_kpt(output,
+                                     0.25, # Confidence Threshold
+                                     0.65, # IoU Threshold
+                                     nc=model.yaml['nc'], # Number of Classes
+                                     nkpt=model.yaml['nkpt'], # Number of Keypoints
+                                     kpt_label=True)
+  with torch.no_grad():
+        output = output_to_keypoint(output)
+  nimg = image[0].permute(1, 2, 0) * 255
+  nimg = nimg.cpu().numpy().astype(np.uint8)
+  nimg = cv2.cvtColor(nimg, cv2.COLOR_RGB2BGR)
+  for idx in range(output.shape[0]):
+      plot_skeleton_kpts(nimg, output[idx, 7:].T, 3)
+
+  return nimg
+
+
